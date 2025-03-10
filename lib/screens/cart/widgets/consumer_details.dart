e6a78bd18bd0b623f0a2421179c2de869ac8908e
julian34 <julianallen111@gmail.com>
2025-03-10 18:53:50 +0900

    Update Cart Screen : TextField Customer Name

--- lib/screens/cart/widgets/consumer_details.dart
+++ lib/screens/cart/widgets/consumer_details.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:pos_coffee_shop/core/theme.dart';
 import 'package:provider/provider.dart';
-import 'package:pos_coffee_shop/screens/cart/widgets/box_consumer.dart';
+// import 'package:pos_coffee_shop/screens/cart/widgets/box_consumer.dart';
 // import 'package:pos_coffee_shop/screens/cart/widgets/box_edit_consumer.dart';
 import 'package:pos_coffee_shop/providers/cart_provider.dart';
 
 class ConsumerDetailsTab extends StatelessWidget {
-  const ConsumerDetailsTab({super.key});
+  final controller;
+  const ConsumerDetailsTab({super.key, this.controller});
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CartProvider>(
       builder: (context, provider, child) {
-        return
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: AppColors.color5,
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: SvgCustomApp.getIcon('user'),
+              ),
+              hintText: "Customer Name",
+              prefixIconConstraints: BoxConstraints(
+                maxHeight: 50,
+                maxWidth: 50,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        );
         // BoxEditConsumer()
-        BoxConsumer();
+        // TextField();
       },
     );
   }
