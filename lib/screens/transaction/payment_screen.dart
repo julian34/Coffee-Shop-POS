6a01981c5a0f5ad712371959a7bca081e566e877
julian34 <julianallen111@gmail.com>
2025-03-13 02:31:56 +0900

    Update the Payment Screen: Build a product list container.

--- lib/screens/transaction/payment_screen.dart
+++ lib/screens/transaction/payment_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:pos_coffee_shop/core/theme.dart';
 import 'package:pos_coffee_shop/models/order_model.dart';
 import 'package:pos_coffee_shop/screens/transaction/widgets/payment/bottom_nav_bar.dart';
 import 'package:pos_coffee_shop/screens/transaction/widgets/payment/consumer_details.dart';
 import 'package:pos_coffee_shop/screens/transaction/widgets/payment/custom_appbar.dart';
+import 'package:pos_coffee_shop/screens/transaction/widgets/payment/list_products.dart';
 
 class PaymentScreen extends StatelessWidget {
   final OrderList? orderList;
@@ -23,7 +25,26 @@ class PaymentScreen extends StatelessWidget {
             customerName: orderList!.customerName,
             totalAmount: orderList!.totalAmount,
           ),
-          Flexible(child: Container()),
+          SizedBox(height: 10),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Ordered",
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                ),
+                Divider(color: AppColors.color3),
+              ],
+            ),
+          ),
+          Flexible(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              child: ListProducts(orderItems: orderList!.items),
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavBar(),
