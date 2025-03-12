9166856aaf9f44b64fc7b3bc22dd5455e9b70f18
julian34 <julianallen111@gmail.com>
2025-03-12 21:16:26 +0900

    Create Cart Screen: build a custom_appbar

--- lib/screens/transaction/payment_screen.dart
+++ lib/screens/transaction/payment_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:pos_coffee_shop/models/order_model.dart';
+import 'package:pos_coffee_shop/screens/transaction/widgets/payment/bottom_nav_bar.dart';
+import 'package:pos_coffee_shop/screens/transaction/widgets/payment/custom_appbar.dart';
 
 class PaymentScreen extends StatelessWidget {
-  const PaymentScreen({super.key});
+  final OrderList? orderList;
+  const PaymentScreen({super.key, this.orderList});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(120),
+        child: CustomAppbar(cartId: orderList!.cartId),
+      ),
+      body: Column(),
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 }
