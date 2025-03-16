bb0770767e5ff2b3b09b7b7fc8d072844fd0aa43
julian34 <julianallen111@gmail.com>
2025-03-16 16:23:39 +0900

    Sign the App (For Release Build)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pos_coffee_shop"
+    namespace = "id.sevatech.poscafe"
     //compileSdk = flutter.compileSdkVersion
     //ndkVersion = flutter.ndkVersion
     compileSdk = 35
@@ -26,7 +26,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pos_coffee_shop"
+        // applicationId = "id.sevatech.poscafe"
+        applicationId "id.sevatech.poscafe"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
