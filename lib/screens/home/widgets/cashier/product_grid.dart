4502b4357faf42b035df944a5816700964c1559a
julian34 <julianallen111@gmail.com>
2025-03-18 02:00:55 +0900

    Update Cashier Home Screen

--- lib/screens/home/widgets/cashier/product_grid.dart
+++ lib/screens/home/widgets/cashier/product_grid.dart
@@ -42,27 +42,173 @@ class ProductGridWidget extends StatelessWidget {
 
         List<Product> products = snapshot.data!;
 
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SizedBox(
-            height:
-                MediaQuery.of(context).size.height * 0.7, // Set a fixed height
-            child: GridView.builder(
-              physics: BouncingScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.75,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
-                return _buildProductCard(context, product);
-              },
-            ),
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.75,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            mainAxisExtent: 265,
           ),
+          padding: EdgeInsets.all(16),
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          itemCount: products.length,
+          itemBuilder: (context, index) {
+            final product = products[index];
+            return GestureDetector(
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(50, 158, 158, 158),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 15),
+                        Center(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(125),
+                            child: Image.network(
+                              product.image,
+                              height: 120,
+                              width: 120,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Text(
+                            product.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                            ),
+                            maxLines: 1,
+                            textAlign: TextAlign.left,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Row(
+                            children: [
+                              Text(
+                                product.category,
+                                style: TextStyle(
+                                  color: AppColors.color6,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              const Spacer(),
+                              SvgCustomApp.getIcon('star', h: 15, w: 15),
+                              const SizedBox(width: 4),
+                              Text(
+                                '4.5',
+                                style: TextStyle(
+                                  color: AppColors.color6,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Text(formatCurrency(product.price)),
+                        ),
+                      ],
+                    ),
+                    // const Padding(
+                    //   padding: const EdgeInsets.only(left: 150, top: 10),
+                    //   child: SvgCustomApp.getIcon('heart'),
+                    // ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Material(
+                        color: AppColors.primary,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(18),
+                          bottomRight: Radius.circular(18),
+                        ),
+                        child: IconButton(
+                          onPressed: () {
+                            Provider.of<CartProvider>(
+                              context,
+                              listen: false,
+                            ).addToCart(
+                              CartItem(
+                                productId: product.id,
+                                name: product.name,
+                                price: product.price,
+                                image: product.image,
+                              ),
+                            );
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("${product.name} added to cart!"),
+                                duration: Duration(seconds: 2),
+                                action: SnackBarAction(
+                                  label: "Undo",
+                                  onPressed: () {
+                                    Provider.of<CartProvider>(
+                                      context,
+                                      listen: false,
+                                    ).updateQuantity(product.id, 0);
+                                    // cartProvider.updateQuantity(cartId, product.id, 0);
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                          icon: SvgCustomApp.getIcon(
+                            'add',
+                            c: AppColors.color5,
+                          ),
+                        ),
+                        // child: InkWell(
+                        //   child: Padding(
+                        //     padding: EdgeInsets.all(8),
+                        //     child:
+                        //   ),
+                        // ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         );
+
+        // return Padding(
+        //   padding: const EdgeInsets.all(16.0),
+        //   child: SizedBox(
+        //     height:
+        //         MediaQuery.of(context).size.height * 0.7, // Set a fixed height
+        //     child: GridView.builder(
+        //       physics: BouncingScrollPhysics(),
+        //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //         crossAxisCount: 2,
+        //         childAspectRatio: 0.75,
+        //         crossAxisSpacing: 10,
+        //         mainAxisSpacing: 10,
+        //       ),
+        //       itemCount: products.length,
+        //       itemBuilder: (context, index) {
+        //         final product = products[index];
+        //         return _buildProductCard(context, product);
+        //       },
+        //     ),
+        //   ),
+        // );
       },
     );
   }
